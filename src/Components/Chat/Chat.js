09c8e1308cf10f181557cacb20a29a787350bcc8
BananaKlit <yhylhl.10@gmail.com>
2024-05-05 02:23:21 +0100

    added user type detection in chat component

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -5,17 +5,26 @@ import { useState } from 'react'
 
 
 export default function Chat({chat,setChat}){
+  const[isTyping,setIsTyping]=useState(false)
 
     const[textMessage,setTextMessage]=useState(null)
     const messageChange=(e)=>{
+      if(isTyping===true){
         setTextMessage(e.target.value)
+        setTimeout (()=>setIsTyping(false),6000)
+      }else{
+      setIsTyping(true);
+      setTextMessage(e.target.value)
+      setTimeout (()=>setIsTyping(false),6000)
+      }
+      
 
     }
     const addMessage = (e) => {
         if (e.key === "Enter") {
           setChat({
             ...chat,
-            messages: [...chat.messages, { message: textMessage, sender: "Founder", timestamp: "11:00" }]
+            messages: [...chat.messages, { message: textMessage, sender: "Paul", timestamp: "11:00" }]
           });
           setTextMessage("")
         }
@@ -28,7 +37,8 @@ return(
         <ul>
         {chat?chat.messages.map((message,i)=>{
             return(<li  key={i}>{"("+message.timestamp+")"}<p><b>{message.sender+": "}</b>{message.message} </p></li>)
-        }):null}</ul>
+        }):null}
+        {isTyping?<p>Paul is typing</p>:null}</ul>
         <input type='textArea' value={textMessage} onChange={(e)=>{messageChange(e)}} onKeyDown={addMessage }/>
             
     </div>
