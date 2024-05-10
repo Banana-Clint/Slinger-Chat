9cd00858dd799081a40c0b5de5e0a7bb671b684e
BananaKlit <yhylhl.10@gmail.com>
2024-05-10 01:35:06 +0100

    changing UX and UI Design for smoother experience in the room view component-ongoing

--- src/Components/Room/Room.js
+++ src/Components/Room/Room.js
@@ -1,6 +1,6 @@
 
 import"./Room.css"  
-import User from "../User/User.js"
+// import User from "../User/User.js"
 import { useState } from "react"
 import Mail from '../Images/Mail Box.png' 
 import LobbyImg from '../Images/Lobby.png' 
@@ -18,14 +18,14 @@ export default function Room(){
         <div className="Dashboard-Wrapper">
         <div className="Dashboard">
         <div className="Dashboard-Tools">
-        <User username={user} />
+   {/* {     <User username={user} />
         <button>+friend+</button>
         <div className="Join-Room">
             <div className="Input-Wrapper">
         <input type= "text" placeholder="ROOM" /> </div>
                     <div className="Buttons-Wrapper"> <button>Join</button> <button id="addRoomButton">+</button></div>
         
-            </div>
+            </div>} */}
             <div className="NavButtons-Wrapper"><button onClick={()=>setLobbyType('Direct-Messages')}><img src={Mail}/> <span style={{
             position: 'absolute', 
             top: '50%', 
